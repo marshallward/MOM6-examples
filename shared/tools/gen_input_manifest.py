4b93d13c6102ca539b96c61b7cc40d4d9158be7b
Marshall Ward <marshall.ward@noaa.gov>
2025-03-07 14:00:05 -0500

    gen_input_manifest.py: Experiment input file list
    This script is a command line too which attempts to generate the list of
    input files for a given experiment.
    
    Information is taken from both input.nml and MOM parameter files to
    reconstruct the list of input files, along with any expected FMS files.
    Any information in INPUT (or perhaps the param's INPUTDIR) is assumed to
    be a symbolic link and is moved over by the associated Makefile.
    
    This script is far from perfect, and it is perhaps impossible to ever
    create such a script, but it should be sufficient for this repo.

---
+++ shared/tools/gen_input_manifest.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+import argparse
+import os
+import shutil
+import sys
+
+from parse_mom_input import parse_mom6_param
+
+# Use the local version of f90nml
+tools_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.join(tools_path, 'f90nml'))
+import f90nml
+
+# Verify output
+# TODO: Redo with argparse
+
+if len(sys.argv) != 2:
+    sys.exit('Error: Experiment path missing.')
+
+expt_path = sys.argv[1]
+if not os.path.isdir(os.path.abspath(expt_path)):
+    sys.exit('Error: path "{}" not found.'.format(expt_path))
+
+if not os.path.isfile(os.path.join(expt_path, 'input.nml')):
+    sys.exit('Error: no "input.nml" in "{}"'.format(expt_path))
+
+
+# All FMS files must have `input.nml.
+manifest = ['input.nml']
+
+# The following are standard FMS inputs.
+FMS_tables = [
+    'diag_table',
+    'data_table',
+    'field_table',
+]
+
+manifest += [
+    f for f in FMS_tables if os.path.isfile(os.path.join(expt_path, f))
+]
+
+
+# Input directories are optional
+if os.path.isdir(os.path.join(expt_path, 'INPUT')):
+    manifest += ['INPUT']
+
+# NOTE: MOM6 may define separate directories for restarts.  This is not yet
+# handled.
+
+
+# Parse parameter inputs
+input_nml = f90nml.read(os.path.join(expt_path, 'input.nml'))
+
+for grp in ('MOM_input_nml', 'SIS_input_nml', 'ice_ocean_driver_nml'):
+    params = input_nml.get(grp, {}).get('parameter_filename', [])
+    if isinstance(params, str):
+        params = [params]
+    manifest += params
+
+
+# TODO: Alistair points out that other inputs may exist outside of INPUT.
+#   It may be so generic that this script could be unusable.
+
+# XXX: This is kind of dopey but just testing it out...
+param_path = os.path.join(expt_path, 'MOM_parameter_doc.all')
+with open(param_path) as param_file:
+    params = parse_mom6_param(param_file)
+
+coord_config = params['COORD_CONFIG']
+if coord_config == 'file':
+    inputdir = params['INPUTDIR']
+
+    if inputdir == '.':
+        coord_file = params['COORD_FILE']
+        coord_path = os.path.join(expt_path, coord_file)
+
+        if os.path.isfile(coord_path):
+            manifest += [coord_file]
+
+
+# Output results in a shell-friendly format.
+print(' '.join(manifest))
