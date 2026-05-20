aeacb959c79c334e233227f0d0f049da12bba7d1
Marshall Ward <marshall.ward@noaa.gov>
2026-05-20 16:39:18 +0000

    config.mk flag update
    More polished sample configuration flags

--- ocean_only/sample.config.mk
+++ ocean_only/sample.config.mk
@@ -2,9 +2,20 @@ CC = nvc
 MPICC = mpicc
 FC = nvfortran
 MPIFC = mpifort
-FCFLAGS = -g -O0 -mp=gpu -stdpar=gpu -gpu=mem:separate -Mnofma -Minfo=all
-LDFLAGS = -mp=gpu
 PYTHON = python3
 
+FCFLAGS = -g -O0 -mp=gpu -stdpar=gpu -gpu=mem:separate \
+  -Mnovect -Mnofma -Minfo=all
+
+# Currently required by NVIDIA GPU in MOM_continuity_PPM.F90
+FCFLAGS += -Minline=name:flux_elem,name:flux_elem_OBC,name:ratio_max
+
 # FMS cannot currently accept -stdpar=gpu
-FMS_FCFLAGS = -g -O0 -mp=gpu -gpu=mem:separate -Mnofma -Minfo=all
+FMS_FCFLAGS = -g -O0 -mp=gpu -acc=gpu -gpu=mem:separate \
+	-Minfo=all
+
+LDFLAGS = -mp=gpu -acc=gpu
+
+## Optionally set the compute capability
+#FCFLAGS += -gpu=cc90
+#LDFLAGS += -gpu=cc90
